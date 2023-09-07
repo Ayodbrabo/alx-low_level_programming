e69932ebfd17cf13cf6d4c6d6b95192ff439d0c5
Ayo Okonye <ayookonye@gmail.com>
2023-09-07 06:03:35 -0700

    done

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 
-int main(void) {
-	put("Programming is like building a multilingual puzzle");
-	return(0);
+/**
+ * main - main block
+ * Return: 0
+ */
+
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
 }
