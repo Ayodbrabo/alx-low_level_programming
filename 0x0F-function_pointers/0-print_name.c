2e4f755818ee09b607bc16499edadbb1d527d04a
Ayo Okonye <ayookonye@gmail.com>
2023-10-11 05:33:05 -0700

    0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -2,13 +2,13 @@
 #include "function_pointers.h"
 /**
 * print_name - print the name
-* @name: is the name 
-* @f: is a  pointer to function 
+* @name: is the name
+* @f: is a  pointer to function
 */
 void print_name(char *name, void (*f)(char *))
 {
-    if (name != NULL && f != NULL)
-    {
-        f(name);
-    }
+if (name != NULL && f != NULL)
+{
+f(name);
+}
 }
