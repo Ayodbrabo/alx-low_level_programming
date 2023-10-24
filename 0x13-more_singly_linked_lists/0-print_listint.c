4ada58b6ab4608c090648334264a6ff44b5007ec
Ayo Okonye <ayookonye@gmail.com>
2023-10-24 14:06:13 -0700

    0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -3,18 +3,13 @@
 /**
 * print_listint - Prints all elements of a listint_t list.
 * @h: A pointer to the head of the list.
-* Return: The number of nodes in the list.
+* Return: The number of nodes.
 */
 size_t print_listint(const listint_t *h)
 {
-size_t node_count = 0;
+if (h == NULL)
+return (0);
 
-while (h != NULL)
-{
 printf("%d\n", h->n);
-node_count++;
-h = h->next;
-}
-
-return (node_count);
+return (1 + print_listint(h->next));
 }
